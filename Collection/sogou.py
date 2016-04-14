caa9b28f58370779cfad35b3748b687f5be916fe
Tch1993 <624512973@qq.com>
2016-04-14 15:28:40 +0800

    delete

--- Collection/sogou.py
+++
@@ -1,5 +0,0 @@
-import re, collection
-class sogou(collection.collection):
-	def __init__(self, queue):
-		super(sogou, self).__init__(queue, self.__class__.__name__);
-		
--- proxy.py
+++
@@ -1,9 +0,0 @@
-from pil import image
-import sys
-image_name = './seccode.jpg';
-im = Image.open(image_name)
-im = im.filter(ImageFilter.MedianFilter())
-enhancer = ImageEnhance.Contrast(im)
-im = enhancer.enhance(2)
-im = im.convert('1')
-im.show()
